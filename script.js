c06b40e24de1e1eb18c1ccb8a838547f59b07b53
Saki Reid <saki.reid@gmail.com>
2021-03-17 16:49:23 +0100

    added in apis list into script

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch('https://randomuser.me/api/')
+.then((response) => response.json())
+.then((response) => console.log(response))
+
+fetch('https://www.boredapi.com/api/activity')
+.then((response) => response.json())
+.then((response) => console.log(response))
+
+fetch('https://api.agify.io?name=tropical')
+.then((response) => response.json())
+.then((response) => console.log(response))
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+.then((response) => response.json())
+.then((response) => console.log(response))
+
+fetch('https://api.imgflip.com/get_memes')
+.then((response) => response.json())
+.then((response) => console.log(response))
+
+
