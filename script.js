bba2c17c898b9107d977ad22ba0ffaca1820c840
Saki Reid <saki.reid@gmail.com>
2021-03-18 11:26:03 +0100

    cleaned up code so write-function was used and inside one form

--- script.js
+++ script.js
@@ -9,20 +9,17 @@ const todo = document.getElementById("todo");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    grabActivity();
+    grabAge();
+})
+
+function grabActivity(){
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
+    console.log(data)
     fetch(`https://www.boredapi.com/api/activity?type=${data.type}`)
     .then((response) => response.json())
     .then((json) => writeText(todo, json.activity))
-    grabAge()
-})
-
-function writeText(element, text) {
-    element.innerHTML = text;
-}
-
-function grabActivity(){
-  
 }
 
 function grabAge(){
@@ -30,12 +27,13 @@ function grabAge(){
   userName.innerHTML = inputValue
   fetch("https://api.agify.io?name="+inputValue)
   .then((response) => response.json())
-   /*.then((response) => console.log(response))*/
-  .then((response)=> {
-      let result= response.age
-      ageParagraph.innerHTML = result
-  })
+    .then((response)=> writeText(ageParagraph, response.age))
 }
+
+function writeText(element, text) {
+    element.innerHTML = text;
+}
+
 
 
 /*fetch('https://randomuser.me/api/')
